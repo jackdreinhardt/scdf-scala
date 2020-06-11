694fce2ce52c313c4482f420a9a6f662dfcd896f
Jack Reinhardt <reinh240@umn.edu>
2020-06-11 14:12:10 -0500

    legacy

--- src/main/scala/com/example/scdfscala/PassThroughLog.scala
+++ src/main/scala/com/example/scdfscala/PassThroughLog.scala
@@ -1,23 +1,23 @@
 package com.example.scdfscala
 
-import java.util.function.Function
-
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.cloud.stream.annotation.{EnableBinding, StreamListener}
+import org.springframework.cloud.stream.messaging.Processor
+import org.springframework.messaging.handler.annotation.SendTo
 
 @SpringBootApplication
+@EnableBinding(classOf[Processor])
 class PassThroughLog {
 
-  @Bean
-  def passthroughlog(): Function[String, String] = {
-    input: String => {
-      println(s"Received input `$input`")
-      input
-    }
+  @StreamListener(value = "input")
+  @SendTo(Array("output"))
+  def passthroughlog(input: String): String = {
+    println(s"Received input `$input`")
+    input
   }
 }
 
 object PassThroughLog {
-  def main(args: Array[String]): Unit = SpringApplication.run(classOf[PassThroughLog], args: _ *)
+  def main(args: Array[String]): Unit = SpringApplication.run(classOf[PassThroughLog], args: _*)
 }
